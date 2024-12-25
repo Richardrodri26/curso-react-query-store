643c5bc3528f075edd6c2384c35ecb34d372554e
RichardRodri26 <richardmanuel811@gmail.com>
2024-12-25 18:43:53 -0500

    finish section 8

---
+++ src/plugins/TanStackProvider.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const TanStackProvider = ({ children }: Props) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
